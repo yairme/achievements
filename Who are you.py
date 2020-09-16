5547c16ee35054fc6a3b4d529770a0e937eb9145
yairme <yairstward2013@gmail.com>
2020-09-16 12:21:52 +0200

    Add files via upload

---
+++ Who are you.py
@@ -0,0 +1,24 @@
+"verses"
+
+vers1 = ("leuk je te ontmoeten, laat me het samenvatten")
+vers2 = ("jouw naam is")
+vers3 = ("je bent:")
+vers4 = ("Oud")
+vers5 = ("en je bent jarig")
+
+"Vragen"
+
+print("Hello, You! Ik ben Yair!")
+print("Wie ben jij?")
+name = input("Naam: ")
+print = ("hoe oud ben jij?")
+age = input("leeftijd: ")
+print = ("Oh cool, ik ben 19 jaar oud, mijn verjaardag was op de 10e, wanneer was / is die van jou?")
+birthday = input("verjaardag: ")
+
+"samenvatten"
+
+print(vers1)
+print(vers2 + name)
+print(vers3 + age + vers4)
+print(vers5 + birthday)
