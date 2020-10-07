706dcd2c8c1df9a9c1836ab0e0928c714ab0fdd7
yairme <yairstward2013@gmail.com>
2020-10-07 14:08:54 +0200

    shuffles

--- Shuffle_Return_AC.py
+++ Shuffle_Return_AC.py
@@ -1,21 +1,11 @@
 
-def Dialog(x):
-   return 5 * x
+import random
 
-print(Dialog(7))
-print(Dialog(10))
-print(Dialog(12))
+def List(names):
+    randomised = ''.join(random.sample(names, len(names)))
+    print(randomised)
+    
 
-def greet(name, msg):
-    print(f"Hello {name} , {msg}" )
-greet("Yair", "Good morning!!")
+names = input()
+List(names)
 
-def Line(name, msg="Good morning."):
-    print(f"Hello {name} , {msg}" )
-Line("Yair")
-Line("Mikey", "How's it going?")
-
-def List(*names):
-    for name in names:
-        print("Hello", name)
-List("Yair","Anthony","Arno","Logan","Gino","Lily")
